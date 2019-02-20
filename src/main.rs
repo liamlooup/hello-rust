5f0566fc26e5f30c0368422a81dcd9a08b8c0cd5
liam looup <liam.looup@yandex.com>
2019-02-20 16:25:31 -0600

    web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    let server = tiny_http::Server::http("0.0.0.0:5099").unwrap();
+
+    loop {
+        let request = match server.recv() {
+            Ok(rq) => rq,
+            Err(e) => { println!("error: {}", e); break }
+        };
+
+        let response = tiny_http::Response::empty(200);
+        let _ = request.respond(response);
+    }
 }
