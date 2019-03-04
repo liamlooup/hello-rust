64272c8af1525849bdd823a13bfb0234f36f661e
liam looup <liam.looup@yandex.com>
2019-03-04 16:41:25 -0600

    fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 START=`date +%s`
 while [ $(( $(date +%s) - 2400 )) -lt $START ]; do
-    curl localhost:5099 -v
-    sleep `jot -r 1 10 100`
+    curl localhost:5099
+    sleep `echo $(( ( RANDOM % 100 )  + 1 ))`
 done
